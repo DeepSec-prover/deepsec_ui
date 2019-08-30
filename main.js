4a9e2e501fca15b7e670d79fa942f7007b441f51
Victor Yon <victor.yon@loria.fr>
2019-08-30 10:49:59 +0200

    Setup result view

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
