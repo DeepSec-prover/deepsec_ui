182c7223efa5039ed684c2d1008fa2c3b87961ec
Victor Yon <victor.yon@loria.fr>
2019-09-04 17:21:45 +0200

    Default load the example result

--- src/views/home.js
+++ src/views/home.js
@@ -2,11 +2,18 @@ const { ipcRenderer } = require('electron')
 const jsonProcessToHtml = require('../tools/process-parser')
 const fs = require('fs')
 
-// Wait to receive a path to a result file, then show it on the page
-ipcRenderer.on('result:show', (event, filePath) => {
+function showResult (filePath) {
   // TODO handle parsing errors and bad path
   const result = JSON.parse(fs.readFileSync(filePath, 'utf8'))
 
   document.getElementById('process').innerText =
     jsonProcessToHtml(result.process1, result.atomic_data)
+}
+
+// Wait to receive a path to a result file, then show it on the page
+ipcRenderer.on('result:show', (event, filePath) => {
+  showResult(filePath)
 })
+
+// TODO remove if no debug
+showResult('mock-data/process_example.json')
