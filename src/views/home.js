ddf74948015de839ef6c3e1c3458c821d4ca3dd5
Victor Yon <victor.yon@loria.fr>
2019-09-05 11:54:15 +0200

    Process formatting with PrismJS

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,15 @@
 const { ipcRenderer } = require('electron')
-const jsonProcessToHtml = require('../tools/process-parser')
+const formatProcess = require('../tools/process-parser')
 const fs = require('fs')
+require('../../static/js/prism') // TODO use npm package ?
 
 function showResult (filePath) {
   // TODO handle parsing errors and bad path
   const result = JSON.parse(fs.readFileSync(filePath, 'utf8'))
 
-  document.getElementById('process').innerText =
-    jsonProcessToHtml(result.process1, result.atomic_data)
+  document.getElementById('process').innerHTML =
+    formatProcess(result.process1, result.atomic_data)
+
 }
 
 // Wait to receive a path to a result file, then show it on the page
