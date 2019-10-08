59a4075d4a4717af6af58947609fd0f10d0fe0ea
Victor Yon <victor.yon@loria.fr>
2019-10-08 17:18:01 +0200

    Init results page

--- src/views/AllResults.vue
+++ src/views/AllResults.vue
@@ -1,12 +1,51 @@
 <template>
   <div>
-    All runs
+    <h2>Results</h2>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="Date"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="Address">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'all-results'
+    name: 'all-results',
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }]
+      }
+    }
   }
 </script>
 
