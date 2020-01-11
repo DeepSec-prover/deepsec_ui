b5d306a1c90fe221d4a745060fda8a91dfce2fcd
Vincent Cheval <vincent.cheval@icloud.com>
2020-01-11 22:38:25 +0100

    First generation of menu. Need to be check on Linux

--- src/electron-menu.js
+++ src/electron-menu.js
@@ -1,4 +1,4 @@
-import { app } from 'electron'
+import { app, shell } from 'electron'
 import logger from 'electron-log'
 import { openResultFileMain } from './util/open-files-dialogs'
 import settings from '../settings'
@@ -10,72 +10,120 @@ import settings from '../settings'
  * @param {Object} mainWindow The main windows reference (for IPC)
  * @returns {{submenu: *[], label: string}[]}
  */
+
+const isMac = process.platform === 'darwin'
+
 function mainMenuTemplate (mainWindow) {
-  const menuTemplate = [
-    {
-      label: 'File',
-      submenu: [
-        {
-          label: 'Open',
-          accelerator: 'CmdOrCtrl+O',
-          click () {
-            openResultFileMain().then(filePath => {
-              // Send file path only because full object is too big
-              logger.debug('Send IPC message : result:show')
-              mainWindow.webContents.send('result:show', filePath)
-            }).catch((_) => {
-              // Nothing to do if canceled or bad value
-            })
-          }
-        },
-        {
-          label: 'Quit',
-          accelerator: 'CmdOrCtrl+Q',
-          click () {
-            app.quit()
-          }
-        },
-        { type: 'separator' },
-        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', role: 'undo' },
-        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
-        { type: 'separator' },
-        { label: 'Zoom In', accelerator: 'CmdOrCtrl+numadd', role: 'zoomIn' },
-        { label: 'Zoom Out', accelerator: 'CmdOrCtrl+numsub', role: 'zoomOut' }
-      ]
-    }
-  ]
 
-  // =================== MacOS basic actions ===================
-  if (process.platform === 'darwin') {
-    menuTemplate[0].submenu = menuTemplate[0].submenu.concat(
-      [
+  // Edit Menu
+  let editMenu = {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'selectAll' }
+    ]
+  }
+
+  let fileMenu = {
+    label: 'File',
+    submenu: [
+      { label: 'New run', accelerator: 'CmdOrCtrl+N' },
+      { label: 'Cancel all runs' },
+      { label: 'View all results', accelerator: 'CmdOrCtrl+R' },
+      { type: 'separator' },
+      { label: 'Export results', accelerator: 'CmdOrCtrl+E' },
+      { label: 'Import results', accelerator: 'CmdOrCtrl+I' },
+      { type: 'separator' },
+      ...(isMac ? [
+        { role : 'close' }
+      ] : [
+        { role : 'about' },
+        { label : 'License' },
+        { label : 'Version 1.0', enabled: false },
+        { label : 'Check for update' },
         { type: 'separator' },
-        { label: 'Cut', accelerator: 'CmdOrCtrl+X', role: 'cut' },
-        { label: 'Copy', accelerator: 'CmdOrCtrl+C', role: 'copy' },
-        { label: 'Paste', accelerator: 'CmdOrCtrl+V', role: 'paste' },
-        { label: 'Select All', accelerator: 'CmdOrCtrl+A', role: 'selectAll' }
+        { label : 'Settings', accelerator: 'Ctrl+,' },
+        { role: 'quit' }
       ])
+    ]
   }
 
-  // ==================== Development Tools ====================
-  if (settings.devTools.menu) {
-    menuTemplate.push(
-      {
-        label: 'Dev Tools',
-        submenu: [
-          {
-            label: 'Toggle inspector',
-            accelerator: process.platform === 'darwin' ? 'Command+I' : 'F12',
-            role: 'toggleDevTools'
-          },
-          {
-            label: 'Reload',
-            accelerator: process.platform === 'darwin' ? 'Command+R' : 'F5',
-            role: 'reload'
-          }
-        ]
-      })
+  let viewMenu = {
+    label: 'View',
+    submenu: [
+      { role: 'toggleDevTools', accelerator: isMac ? 'Shift+Command+I' : 'F12' },
+      { role: 'reload', accelerator: isMac ? 'Shift+Command+R' : 'F5'},
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
   }
+
+  let windowMenu = {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [] : [
+        { role: 'close' }
+      ])
+    ]
+  }
+
+  let helpMenu = {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Official Website',
+        click: async () => {
+            await shell.openExternal('https://deepsec-prover.github.io')
+          }
+      },
+      { label: 'Documentation' },
+      {
+        label: 'Report a bug',
+        click: async () => {
+          await shell.openExternal('https://github.com/DeepSec-prover/deepsec_ui/issues/new')
+        }
+      }
+    ]
+  }
+
+  let appMenu = {
+    label: app.getName(),
+    submenu: [
+      { role: 'about' },
+      { label: 'License' },
+      { label: 'Version 1.0', enabled: false },
+      { label: 'Check for update' },
+      { type: 'separator' },
+      { label : 'Settings', accelerator: 'Cmd+,' },
+      { type: 'separator' },
+      { role: 'services', submenu: [] },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }
+
+  const menuTemplate = [fileMenu,editMenu,viewMenu,windowMenu,helpMenu]
+
+  if (isMac) {
+    menuTemplate.unshift(appMenu)
+  }
+
   return menuTemplate
 }
 
