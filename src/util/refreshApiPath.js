225c148e951a943a25e2315e2815f48c19f3bcd6
Vincent Cheval <vincent.cheval@icloud.com>
2020-01-12 23:52:28 +0100

    Automatic detection of deepsec_api when it is in the environment variable PATH

--- src/util/refreshApiPath.js
+++ src/util/refreshApiPath.js
@@ -2,6 +2,9 @@ import logger from 'electron-log'
 import userSettings from 'electron-settings'
 import { isEmptyOrBlankStr, isFile } from './misc'
 import { ApiGetConfig } from '../deepsec-api/ApiGetConfig'
+import { defaultUserSettings } from './default-user-settings'
+
+let which = require('which')
 
 /**
  * Check the current Api path and refresh the results directory path
@@ -10,16 +13,25 @@ import { ApiGetConfig } from '../deepsec-api/ApiGetConfig'
  */
 export function refreshApiPath (mainWindow) {
   // Check the Deepsec API path
-  const apiPath = String(userSettings.get('deepsecApiPath'))
+  let apiPath = String(userSettings.get('deepsecApiPath'))
 
   if (isEmptyOrBlankStr(apiPath)) {
-    logger.warn('DeepSec API path is not set')
-    mainWindow.webContents.send('notification:show',
-                                'DeepSec API path is not set',
-                                'Please set it in the setting page.',
-                                'warning',
-                                'init',
-                                { name: 'settings' })
+    // Try to see if we can find it in path
+    let deepsecApi = which.sync('deepsec_api', { nothrow: true })
+    if (deepsecApi === null || isEmptyOrBlankStr(deepsecApi)) {
+      logger.warn('DeepSec API path is not set')
+      mainWindow.webContents.send('notification:show',
+                                  'DeepSec API path is not set',
+                                  'Please set it in the setting page.',
+                                  'warning',
+                                  'init',
+                                  { name: 'settings' })
+    } else {
+      logger.info(`DeepSec API path detected: ${apiPath}`)
+      userSettings.set("deepsecApiPath", deepsecApi)
+      const apiGetConf = new ApiGetConfig(mainWindow, 'init')
+      apiGetConf.start() // The answer will be catch then the process will close itself
+    }
   } else if (!isFile(apiPath)) {
     logger.warn(`DeepSec API path is set but the path is not valid (${apiPath})`)
     mainWindow.webContents.send('notification:show',
