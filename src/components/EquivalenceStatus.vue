fbcae20f81e23ded4644587bbac7cf1f17699f8c
Vincent Cheval <vincent.cheval@icloud.com>
2020-01-16 23:39:22 +0100

    Fix #63 Found a bug at the same time in DeepSec (commit 038b065) and DeepSec UI (for the display of equivalence status)

--- src/components/EquivalenceStatus.vue
+++ src/components/EquivalenceStatus.vue
@@ -7,28 +7,30 @@
     <template v-if="equivalence.status === 'non_equivalent_message'">
       The recipe
       <spec-code-inline :code="recipeStr"></spec-code-inline>
-      yields the message
+      evaluates to
       <spec-code-inline :code="termStr"></spec-code-inline>
-      on Process {{ equivalence.process_id }}
-      but fails to compute a message on Process {{ (equivalence.process_id % 2) + 1 }}.
+      on process {{ equivalence.process_id }}
+      but fails on process {{ (equivalence.process_id % 2) + 1 }}.
     </template>
 
     <template v-else-if="equivalence.status === 'non_equivalent_equality'">
-      The two recipes
-      <spec-code-inline :code="recipe1Str"></spec-code-inline>
-      and
-      <spec-code-inline :code="recipe2Str"></spec-code-inline>
-      yield the same message
-      <spec-code-inline :code="termEqualStr"></spec-code-inline>
-      on Process {{ equivalence.process_id }}
-      but they yield different messages on Process {{ (equivalence.process_id % 2) + 1 }}
-      (<spec-code-inline :code="term1Str"></spec-code-inline> and <spec-code-inline :code="term2Str"></spec-code-inline> respectively).
+      The test <spec-code-inline :code="recipe1Str + ' = ' + recipe2Str"></spec-code-inline>
+      holds on process {{ equivalence.process_id }}, but not on process {{ (equivalence.process_id % 2) + 1 }}.
+      <ul>
+        <li><spec-code-inline :code="recipe1Str"></spec-code-inline> and <spec-code-inline :code="recipe2Str"></spec-code-inline>
+          evaluate to <spec-code-inline :code="termEqualStr"></spec-code-inline> in process {{ equivalence.process_id }}.
+        </li>
+        <li><spec-code-inline :code="recipe1Str"></spec-code-inline> evaluates to <spec-code-inline :code="term1Str"></spec-code-inline>
+          on process {{ (equivalence.process_id % 2) + 1 }}.
+        </li>
+        <li><spec-code-inline :code="recipe2Str"></spec-code-inline> evaluates to <spec-code-inline :code="term2Str"></spec-code-inline>
+          on process {{ (equivalence.process_id % 2) + 1 }}.
+        </li>
+      </ul>
     </template>
 
     <template v-else>
-      To imitate the attack trace of Process {{ processDisplayedId }}, an {{ nextAction.type }} transition on the channel
-      <spec-code-inline :code="channelStr"></spec-code-inline>
-      should be available in Process {{ processDisplayedId %2 + 1 }} but no more visible actions are available.
+      Impossible to match the visible transition <spec-code-inline :code="actionStr"></spec-code-inline> of process {{ processDisplayedId }}.
     </template>
   </el-card>
 
@@ -37,7 +39,7 @@
 <script>
 import text from '../text-content/text'
 import AtomicRenamer from '../util/AtomicRenamer'
-import { formatCode } from '../util/process-parser'
+import { formatCode, formatAction } from '../util/process-parser'
 import SpecCodeInline from './code/SpecCodeInline'
 
 export default {
@@ -48,7 +50,11 @@ export default {
       type: Object,
       required: true
     },
-    atomic: {
+    atomicUser: {
+      type: AtomicRenamer,
+      required: true
+    },
+    atomicDisplayed: {
       type: AtomicRenamer,
       required: true
     },
@@ -68,28 +74,36 @@ export default {
   },
   computed: {
     recipeStr: function () {
-      return formatCode(this.equivalence.recipe, this.atomic)
+      // For recipes, we do not need to distinguish the atomic renamer since no names occur.
+      return formatCode(this.equivalence.recipe, this.atomicDisplayed)
     },
     termStr: function () {
-      return formatCode(this.equivalence.term, this.atomic)
+      const atomic = (this.processDisplayedId === this.equivalence.process_id) ? this.atomicDisplayed : this.atomicUser
+      return formatCode(this.equivalence.term, atomic)
     },
     recipe1Str: function () {
-      return formatCode(this.equivalence.recipe1, this.atomic)
+      // For recipes, we do not need to distinguish the atomic renamer since no names occur.
+      return formatCode(this.equivalence.recipe1, this.atomicDisplayed)
     },
     recipe2Str: function () {
-      return formatCode(this.equivalence.recipe2, this.atomic)
+      // For recipes, we do not need to distinguish the atomic renamer since no names occur.
+      return formatCode(this.equivalence.recipe2, this.atomicDisplayed)
     },
     termEqualStr: function () {
-      return formatCode(this.equivalence.term_equal, this.atomic)
+      const atomic = (this.processDisplayedId === this.equivalence.process_id) ? this.atomicDisplayed : this.atomicUser
+      return formatCode(this.equivalence.term_equal, atomic)
     },
     term1Str: function () {
-      return formatCode(this.equivalence.term1, this.atomic)
+      const atomic = (this.processDisplayedId === this.equivalence.process_id) ? this.atomicUser : this.atomicDisplayed
+      return formatCode(this.equivalence.term1, atomic)
     },
     term2Str: function () {
-      return formatCode(this.equivalence.term2, this.atomic)
+      const atomic = (this.processDisplayedId === this.equivalence.process_id) ? this.atomicUser : this.atomicDisplayed
+      return formatCode(this.equivalence.term2, atomic)
     },
-    channelStr: function () {
-      return formatCode(this.nextAction.channel, this.atomic)
+    actionStr: function () {
+      // For action, we do not need to distinguish the atomic renamer since no names occur.
+      return formatAction(this.nextAction.action, this.atomic,this.nextAction.last_axiom)
     }
   }
 }
