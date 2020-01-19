5235d2689cda9144e4f432dd27d1bed65a4fb44b
Vincent Cheval <vincent.cheval@icloud.com>
2020-01-19 21:01:09 +0100

    Added a mixin for ActionPopup

--- src/components/ActionPopup.vue
+++ src/components/ActionPopup.vue
@@ -63,9 +63,11 @@ import Vue from 'vue'
 import ProcessModel from '../models/ProcessModel'
 import DialogDrag from 'vue-dialog-drag'
 import 'vue-dialog-drag/dist/vue-dialog-drag.css'
+import positionMixin from './ActionPopup-positionMixin'
 
 export default {
   name: 'action-popup',
+  mixins: [positionMixin],
   components: { RecipeInput, DialogDrag },
   props: {
     action: {
@@ -75,17 +77,6 @@ export default {
     atomic: {
       type: AtomicRenamer,
       require: true
-    },
-    dataPopper: {
-      type: Object
-    },
-    isVisible: {
-      type: Boolean,
-      require: true
-    },
-    initialLeft: {
-      type: Number,
-      require: true
     }
   },
   data () {
@@ -94,13 +85,7 @@ export default {
       recipes: {},
       nbProcessUnfolded: 1,
       validRecipes: false,
-      cacheRecipes: new Map(),
-      dataDrag: null,
-      options: { buttonPin: false, buttonClose: false, left: 0, top: 0, dragCursor: '-moz-grab'},
-      marginDragBot: 25,
-      marginDragTop: 5,
-      marginDragLeft: 15,
-      marginDragRight: 25,
+      cacheRecipes: new Map()
     }
   },
   computed: {
@@ -115,47 +100,6 @@ export default {
     }
   },
   methods: {
-    updatePopper () {
-      let options = this.options
-      this.reconfigure(this.dataPopper,this.dataDrag,options)
-      this.options = Object.assign({},options)
-    },
-    setOffset (data) {
-      this.dataDrag = {
-        left: data.left,
-        top: data.top
-      }
-      let options = this.options
-      this.reconfigure (this.dataPopper,this.dataDrag,options)
-      this.options = Object.assign({},options)
-        // Seems that just updating the value of options does not work. Creating
-        // a completely new option seems to trigger an update correctly.
-        // Trick obtained on Github by the creator of dialog-drag
-    },
-    reconfigure (dataPopper,dataDrag,options) {
-      if (!dataDrag) {
-        dataDrag = { left: 0, top: 0 }
-        this.dataDrag = { left: 0, top: 0 }
-      }
-
-      const width = this.$refs.dragBox.clientWidth
-      const height = this.$refs.dragBox.clientHeight
-
-      if (dataPopper.top + dataDrag.top < this.marginDragTop) {
-        options.top = this.marginDragTop - dataPopper.top
-      } else if (dataPopper.top + dataDrag.top + height > dataPopper.heightWindow - this.marginDragBot) {
-        options.top = dataPopper.heightWindow - this.marginDragBot - height - dataPopper.top
-      } else {
-        options.top = dataDrag.top
-      }
-      if (dataPopper.left + dataDrag.left < this.initialLeft + this.marginDragLeft) {
-        options.left = this.initialLeft + this.marginDragLeft - dataPopper.left
-      } else if (dataPopper.left + dataDrag.left + width > dataPopper.widthWindow + this.initialLeft - this.marginDragRight) {
-        options.left = this.initialLeft + dataPopper.widthWindow - this.marginDragRight - dataPopper.left - width
-      } else {
-        options.left = dataDrag.left
-      }
-    },
     validate () {
       if (this.action.type === 'bang') {
         this.$emit('user-select-action', {
@@ -247,16 +191,6 @@ export default {
     },
     selectedTransitionType () {
       this.checkRecipes()
-    },
-    dataPopper (newVal, _) {
-      let options = this.options
-      this.reconfigure(newVal,this.dataDrag,options)
-      this.options = Object.assign({},options)
-    },
-    isVisible (bool) {
-      if (!bool) {
-        this.dataDrag = { top: 0, left: 0 }
-      }
     }
   }
 }
